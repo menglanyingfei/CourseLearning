ffdba4f10bd31bd38677eb07cab0297b3ce55291
Li Xingyang <lixingyang511@gmail.com>
2018-01-01 15:29:51 +0800

    upload Matlab学习 目录

---
+++ matlab学习/Matlab课程/作业提交/第8章作业/untitled1.m
@@ -0,0 +1,121 @@
+function varargout = untitled1(varargin)
+% UNTITLED1 MATLAB code for untitled1.fig
+%      UNTITLED1, by itself, creates a new UNTITLED1 or raises the existing
+%      singleton*.
+%
+%      H = UNTITLED1 returns the handle to a new UNTITLED1 or the handle to
+%      the existing singleton*.
+%
+%      UNTITLED1('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in UNTITLED1.M with the given input arguments.
+%
+%      UNTITLED1('Property','Value',...) creates a new UNTITLED1 or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before untitled1_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to untitled1_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help untitled1
+
+% Last Modified by GUIDE v2.5 08-Nov-2016 23:45:20
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @untitled1_OpeningFcn, ...
+                   'gui_OutputFcn',  @untitled1_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before untitled1 is made visible.
+function untitled1_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to untitled1 (see VARARGIN)
+
+% Choose default command line output for untitled1
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes untitled1 wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = untitled1_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global signal;
+h = waitbar(0,' Recording .... ');
+Fs=10000;
+for i=1:1
+    data=wavrecord(5*Fs,Fs);  % 5 seconds recording
+    waitbar(i/1,h)
+end
+close (h);
+signal=data;
+
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+global signal;
+Fs=10000;
+signal = 0.99*signal/max(abs(signal));
+if length(signal) ~= 0
+      wavplay(signal,Fs)
+end
+
+function plot_sig()
+global signal;
+Fs = 10000;
+            samp_len = length(signal)/Fs;
+            delta_t = 1/Fs;
+            t = 0:delta_t:(samp_len-delta_t);
+            % display the signal
+ plot(t,signal);xlabel('Time'), ylabel('Amplitude')
+ axis([0 t(length(signal)-1) -1 1 ]);
+
+
+% --- Executes on button press in pushbutton3.
+function pushbutton3_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+plot_sig
+    
